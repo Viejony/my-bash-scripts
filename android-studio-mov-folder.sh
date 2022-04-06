f2815803de256955068b357c9ce55088fb48eb09
Jhonyfer Angarita Moreno <Administrador@WTDISPONIBLE1.BOG.WIDETECH.CO>
2022-04-06 08:47:01 -0500

    Added new script for rename folders in Android Studio environment.

---
+++ android-studio-mov-folder.sh
@@ -0,0 +1,22 @@
+# renames folder in the list
+
+mv_bg_files(){
+    
+    prefix="drawable-"
+    newprefix="$1-"
+    newdirs=(
+        "ldpi"
+        "mdpi"
+        "hdpi"
+        "xhdpi"
+        "xxhdpi"
+        "xxxhdpi"
+    )
+
+    echo "Renamig folders"
+    for t in ${newdirs[@]}; do
+        mv $prefix$t $prefix$newprefix$t
+    done
+}
+
+mv_bg_files $1
