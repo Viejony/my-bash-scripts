544c15018db14e9141e79ad714eae0d5cf79c9a7
Jhonyfer Angarita Moreno <viejony@gmail.com>
2022-04-11 14:46:34 -0500

    Updated scripts content

--- ffmpeg-encode-files.sh
+++ ffmpeg-encode-files.sh
@@ -3,6 +3,10 @@
 files=$(ls | grep ".mp4")
 folder="converted"
 
+# Creates a folder
+mkdir $folder
+
+# Itereates over mp4 files
 for t in ${files[@]}; do
     ffmpeg -i $t -c:v libx265 -crf 26 -preset medium -c:a aac -b:a 128k $folder/$t
 done
