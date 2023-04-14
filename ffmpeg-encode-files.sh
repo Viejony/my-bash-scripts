55b007be70dad6785a92e23c18271a454d946018
Jhonyfer Angarita Moreno <40864812+Viejony@users.noreply.github.com>
2023-04-13 20:24:52 -0500

    Update ffmpeg-encode-files.sh

--- ffmpeg-encode-files.sh
+++ ffmpeg-encode-files.sh
@@ -9,4 +9,6 @@ mkdir $folder
 # Itereates over mp4 files
 for t in ${files[@]}; do
     ffmpeg -i $t -c:v libx265 -crf 26 -preset medium -c:a aac -b:a 128k $folder/$t
+    # Example with Nvidia GPU
+    # ffmpeg -y -vsync 0 -hwaccel cuda -hwaccel_output_format cuda -i $t -c:a copy -c:v av1_nvenc -b:v 5M $folder/$t
 done
